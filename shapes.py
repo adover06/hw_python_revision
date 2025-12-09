131f533bce78d5be83f54ef36fbd0bb75865c6e8
Andrew Dover <monkeypunk811@gmail.com>
2025-12-08 23:32:09 -0800

    updated shapes again

--- shapes.py
+++ shapes.py
@@ -5,18 +5,33 @@ class Base:
         self.size = size
         
 
-class Square(Base): #added the Base class as a parent class
+class Circle(Base): #added the Base class as a parent class
     def __init__(self, x, y, size): #added the y paramerter
         super().__init__(x, y, size) #pass the values to the parent constructor
         
     def shape(self): #returns the shape type
-        return "This is a square"
+        return "This is a circle"
     def draw(self):
-        return f"({self.x}, {self.y})\n{self.size}"
+        return f"""
+({self.x}, {self.y})\n{self.size}
+         , - ~ ~ ~ - ,
+     , '               ' ,
+   ,                       ,
+  ,                         ,
+ ,                           ,
+ ,                           ,
+ ,                           ,
+  ,                         ,
+   ,                       ,
+     ,                  , '
+       ' - , _ _ _ ,  '
+               """
+#I tried to copy the circle text from the homework pdf and from the pdf provided in the announcement but both wont copy the spacing correctly.
 
 def main():
-    s = Square(2, 3, 5)
-    print(s.shape())
-    print((s.x, s.y))
-    print(s.size)
+    c = Circle(1,2,3)
+    print(c.shape())
+    print(c.draw())
+
+
 main()
